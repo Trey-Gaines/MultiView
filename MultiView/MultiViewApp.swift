59f1828c95ea2d75cd4186b83b4f53e1de9f26f2
Trey <88073284+Trey-Gaines@users.noreply.github.com>
2024-08-15 18:29:52 -0400

    Initial Commit

---
+++ MultiView/MultiViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiViewApp.swift
+//  MultiView
+//
+//  Created by Trey Gaines on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiViewTests/MultiViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiViewTests.swift
+//  MultiViewTests
+//
+//  Created by Trey Gaines on 8/15/24.
+//
+
+import Testing
+@testable import MultiView
+
+struct MultiViewTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
