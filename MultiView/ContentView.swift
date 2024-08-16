552dff33fd46462e363266ae99f64d8398362c57
Trey <88073284+Trey-Gaines@users.noreply.github.com>
2024-08-15 20:18:53 -0400

    Reading up a bit

--- MultiView/ContentView.swift
+++ MultiView/ContentView.swift
@@ -10,16 +10,36 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State var streamName = ""
+    @State var currentViews = 0
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+            HStack(spacing: 50) {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+                
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+            }
+            VStack {
+                Text("Add a Stream")
+                    .font(.extraLargeTitle)
+                    .fontWeight(.semibold)
+                TextField("Input link or user", text: $streamName)
+                    .frame(width: 300, height: 50)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .multilineTextAlignment(.center)
+            }
         }
         .padding()
     }
 }
+
+enum TabChoices {
+      //Tabview
+}
+
+
 
 #Preview(windowStyle: .automatic) {
     ContentView()
